7680d87c04c8539180679eac9335a7c172cbea68
wfogame <raiderattackers@gmail.com>
2025-08-16 23:36:30 -0400

    eh

---
+++ random.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  int hello,dummy,cool;
+  float eh,blah,duh;
+
+  printf("%d\n",hello);
+  printf("%d\n",dummy);
+  printf("%d\n",cool);
+  printf("%f\n",eh);
+  printf("%f\n",blah);
+  printf("%f\n",duh);
+}
