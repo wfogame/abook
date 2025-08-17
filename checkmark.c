f141386f31cfb54e902ae74eabab869d23c973dc
wfogame <raiderattackers@gmail.com>
2025-08-16 23:33:09 -0400

    Checkmark printing

---
+++ checkmark.c
@@ -0,0 +1,18 @@
+/*
+ * Height of total structure is six
+ * Width of total structure is eight
+ * 3 6 pattern, so 9 - 1. 
+ *
+ *
+ * */
+#include <stdio.h>
+#include <iostream>
+
+  int main(){
+  printf("       *\n");
+  printf("      *\n");
+  printf("     *\n");
+  printf("*   *\n");
+  printf(" * *\n");
+  printf("  *\n");
+}
